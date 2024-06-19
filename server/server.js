84fa5f1450b8595829fe0ef2070b8c9224361bbe
Lazar Todorovic <vulalaza@gmail.com>
2024-06-19 19:20:05 +0200

    battle aces express proxy server

---
+++ server/server.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+
+var corsOptions = {
+    origin: 'https://zaokret.github.io',
+    optionsSuccessStatus: 200 
+  }
+
+app.use(cors(corsOptions))
+
+app.get('/data', function (req, res) {
+    fetchData().then(data => {
+        res.json(data);
+    })
+});
+
+var port = 3000;
+app.listen(port, function () {
+    console.log(`Battle Aces Proxy Server listening on port ${port}.`);
+});
+
+function fetchData() {
+    return fetch(`https://www.playbattleaces.com/units`)
+        .then((res) => res.text())
+        .then(extractBuildId)
+        .then(urlBuilder)
+        .then((url) => fetch(url))
+        .then((res) => res.json())
+        .then((body) => ({
+            tiers: parseTechTiers(body),
+            units: parseAndSortUnits(body),
+        }));
+}
+
+function urlBuilder(buildId) {
+    return `https://www.playbattleaces.com/_next/data/${buildId}/en-US/units.json`;
+}
+
+function extractBuildId(text) {
+    const regex = /"buildId":\s*"(.*?)"/;
+    const match = text.match(regex);
+    return match ? match[1] : null;
+}
+
+function parseAndSortUnits(body) {
+    const getTechTierNameById = (id) => {
+        const tier = body.pageProps.data.allTechTiers.find(
+            (tier) => tier.techTierId === id
+        );
+        return tier.name;
+    };
+
+    return body.pageProps.data.allUnits
+        .map((unit) => ({
+            id: unit.id,
+            costBandwidth: unit.costBandwidth,
+            costMatter: unit.costMatter,
+            costEnergy: unit.costEnergy,
+            name: unit.name,
+            statDamage: unit.statDamage,
+            statHealth: unit.statHealth,
+            statRange: unit.statRange,
+            statSpeed: unit.statSpeed,
+            techTier: getTechTierNameById(unit.techTier.techTierId),
+            techTierId: unit.techTier.techTierId,
+            unitAbility: unit.unitAbility && unit.unitAbility.name,
+            unitTag: unit.unitTag,
+            slug: unit.slug,
+        }))
+        .sort((a, b) => a.techTierId - b.techTierId);
+}
+
+function parseTechTiers(body) {
+    return body.pageProps.data.allTechTiers.map((tier) => ({
+        name: tier.name,
+        slug: tier.slug,
+        techTierId: tier.techTierId,
+    }));
+}
